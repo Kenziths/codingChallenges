e97a6a81acf7e5d86d48a7fe479caea9d3f42a0a
Kenny S <kennethstrunk@yahoo.com>
2023-07-20 05:27:04 -0600

    second try

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -4,4 +4,10 @@
 // const areaOrPerimeter = function(l , w) {
 //     return l == w ? l*w : 2*(l+w)
 //   };
-  
+
+//You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+//As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+// function reverse(str){
+//     return str.split(" ").reverse().join(" ")
+//   }
