4735c59c59e464ff753fa269f1a989b300cbecd4
Kenny S <kennethstrunk@yahoo.com>
2023-07-21 21:21:16 -0600

    can car make it to gas statin with fuel remaining code wars

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -1,3 +1,5 @@
+
+
 // You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
 // If it is a square, return its area. If it is a rectangle, return its perimeter.
 
@@ -5,9 +7,28 @@
 //     return l == w ? l*w : 2*(l+w)
 //   };
 
+
+
+
+
 //You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
 //As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
 
 // function reverse(str){
 //     return str.split(" ").reverse().join(" ")
+//   }
+
+
+
+
+
+
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return (mpg * fuelLeft) >= distanceToPump
 //   }
