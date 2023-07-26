7e5a5910cf0ffbfda2211f366f74cf0d4ae8aac7
Kenny S <kennethstrunk@yahoo.com>
2023-07-25 19:42:41 -0600

    rock paper scissors game

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -70,3 +70,22 @@
 // For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
 //const quarterOf = m => Math.ceil(m/3);
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// const rps = (p1, p2) => {
+//     if (p1 === p2) return "Draw!"
+//     var rules = {rock: "scissors", paper: "rock", scissors: "paper"}
+//     if (p2 === rules[p1]) {
+//       return "Player 1 won!"
+//     }
+//     else {
+//       return "Player 2 won!"
+//     }
+//   }
