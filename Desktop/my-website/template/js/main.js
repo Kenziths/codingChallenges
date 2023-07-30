ba3cdd22cc4721bc6a09bc7f21cd21585e5e22b0
Kenny S <kennethstrunk@yahoo.com>
2023-07-30 07:43:02 -0600

    sort an array in acesndin order including null and negatives

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -148,3 +148,11 @@
 // function booleanToString(b){
 //     return b.toString()
 //   }
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// function solution(nums){
+//     return (nums || []) .sort (function(a , b){
+//       return a - b
+//     })
+//     }
