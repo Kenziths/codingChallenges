7c6403812f2edf0fcce7b631105f631f33fc3593
Kenny S <kennethstrunk@yahoo.com>
2023-07-31 19:55:12 -0600

    duck duck goose game

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -156,3 +156,9 @@
 //       return a - b
 //     })
 //     }
+
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+// function duckDuckGoose(players, goose) {
+//     return goose ? player[(goose - 1) % players.lenght].name : ''
+//   }
