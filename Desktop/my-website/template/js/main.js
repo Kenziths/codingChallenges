9e57563aac51be20a4c57caa465f7e625fbd8b65
Kenny S <kennethstrunk@yahoo.com>
2023-08-03 19:26:14 -0600

    removing every other element from an array starting with the first index

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -199,3 +199,10 @@
 // function subt(a,b){
 //     return a-b
 // }
+
+//Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// function removeEveryOther(arr){
+//     return arr.filter(function(elem, index){
+//       return index % 2 === 0
+//     })
+// }
