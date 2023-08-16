4b503ecfc493637341c78fb42d53b156a031a641
Kenny S <kennethstrunk@yahoo.com>
2023-08-16 17:28:09 -0600

    fix push an object into an array

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -307,3 +307,8 @@
 //   } 
 //   return true;
 // }
+
+//You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+// let items = []
+// items.push ({a: "b", c: "d"})
