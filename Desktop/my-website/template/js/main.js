db6516ffab16c8b71e94fbe2ffeafd596418a423
Kenny S <kennethstrunk@yahoo.com>
2023-09-27 18:00:12 -0600

    set alarm function

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -407,3 +407,13 @@
 //function binToDec(bin){
 //    return parseInt(bin, 2)
 //  }
+
+//The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// function setAlarm(employed, vacation){
+//     if(employed && !vacation){
+//       return true
+//     }else{
+//       return false
+//     }
+//   }
