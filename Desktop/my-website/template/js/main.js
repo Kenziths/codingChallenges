3e79114be3a3b5a379319369ccf680296cbcf07d
Kenny S <kennethstrunk@yahoo.com>
2023-10-03 18:48:22 -0600

    remove all the explation marks from the end of a sentence

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -440,3 +440,11 @@
 //     }else
 //       return num - 6
 //    }
+//  Remove all exclamation marks from the end of sentence.
+// function remove (s) {  
+//     while(s && s.slice(-1) == "!")
+//       {
+//         s = s.slice(0,-1)
+//       }
+//     return s
+//   }
