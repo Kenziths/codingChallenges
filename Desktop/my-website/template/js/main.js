403c7c090cfe22be7783b786b342129d00039a2d
Kenny S <kennethstrunk@yahoo.com>
2023-10-04 17:14:02 -0600

    reverse a array

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -448,3 +448,9 @@
 //       }
 //     return s
 //   }
+
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// function reverseList(list) {
+//     return list.reverse()
+//    }
