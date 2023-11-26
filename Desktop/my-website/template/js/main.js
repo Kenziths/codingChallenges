671c39dd2bb9b5596f46800c10903e1a843f4637
Kenny S <kennethstrunk@yahoo.com>
2023-11-26 14:21:49 -0700

    comparing vowels to char and then replacing

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -627,3 +627,18 @@
 //       return "not at all"
 //     }
 // }
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// // Return the resulting array.
+// function isVow(a){
+//     for(var i=0;i<a.length;i++){
+//     if(a[i]===97){a[i]="a";}
+//     if(a[i]===101){a[i]="e";}
+//     if(a[i]===105){a[i]="i";}
+//     if(a[i]===111){a[i]="o";}
+//     if(a[i]===117){a[i]="u";}
+//     }
+//     return a 
+//     }
