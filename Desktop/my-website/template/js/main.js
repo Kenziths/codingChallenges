eb3d28641651be43b5e077e2af3b446ec5ed832b
Kenny S <kennethstrunk@yahoo.com>
2023-12-07 19:56:24 -0700

    using arrays

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -684,4 +684,12 @@
 //     }else{
 //       return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won.`
 //   }
+// //   }
+// Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
+// function whoIsPaying(name){
+//     if(name.length <= 2){
+//       return [name]
+//     }else{
+//       return [name, name.slice(0,2)]
+//     }
 //   }
