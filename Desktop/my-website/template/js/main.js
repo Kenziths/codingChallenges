7e3cde8fba351428c4e164afc21bd97ed39f6757
Kenny S <kennethstrunk@yahoo.com>
2024-01-19 20:09:59 -0700

    split filter practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -808,3 +808,9 @@
 // //     return word[0].toUpperCase() + word.slice(1)
 // //   }
 // Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+//Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// function shortcut (str) {
+//     return str.split('').filter(function(e) {
+//        return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1
+//     }).join('')
+//   }
