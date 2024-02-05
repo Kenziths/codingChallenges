7c209247de644be6838c530b31f5e127491e3c56
Kenny S <kennethstrunk@yahoo.com>
2024-02-05 16:39:19 -0700

    .sort method practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -867,3 +867,8 @@
 //     }
 //   return returnArr
 // }
+//Time to test your basic knowledge in functions! Return the odds from a list:
+// function odds(values){
+//   // arrow it
+//   return values.filter(values => values % 2 !== 0);
+// }
