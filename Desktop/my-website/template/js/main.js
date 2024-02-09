b0c75f53e68947f2ddfa1b937ab10890da9746c0
Kenny S <kennethstrunk@yahoo.com>
2024-02-08 20:21:17 -0700

    sql order practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -891,3 +891,8 @@
 // FROM people 
 // WHERE age > 50
 // ORDER BY age DESC 
+
+// // Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
+// SELECT * 
+// FROM companies
+// ORDER BY employees desc
