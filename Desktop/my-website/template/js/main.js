8e6ca90e1f90c720b4aef2777b320b8c6b55e6bc
Kenny S <kennethstrunk@yahoo.com>
2024-02-13 19:21:41 -0700

    SELECT and FILTERING queries

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -926,3 +926,7 @@
 //       volume,
 //       width,
 //       height;
+// Select names, and countries of origin of all the travelers, excluding anyone from Canada, Mexico, or The US.
+// SELECT name,country
+// FROM travelers
+// WHERE country NOT IN ('Canada','Mexico','USA')
