4c04678c0a76783fe6f40fc86ef68310adbb7fcf
Kenny S <kennethstrunk@yahoo.com>
2024-06-18 17:33:34 -0600

    array pratice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -995,3 +995,18 @@
 // 	return string.split(" ")
 
 // }
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+// function well(x){
+//   let goodIdeas = 0;
+//     for(let i = 0; i < x.length; i += 1){
+//       if(x[i]== "Good"){
+//         goodIdeas += 1
+//       }
+//       if(goodIdeas >= 3){
+//         return "I small a series!"
+//       }
+//     }
+//   return goodIdeas >= 1 ? "publish!" : "Fail"
+// }
