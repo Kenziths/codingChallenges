9e5072d23c7008d1254e135f24f3fb9748764c18
Kenny S <kennethstrunk@yahoo.com>
2024-10-06 11:11:50 -0600

    array practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -1027,4 +1027,14 @@
 //     }
 //   }
 //   return null
+// // }
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+
+// function grow(x){
+//   let sum = 1
+//   for(let i = 0; i < x.length; i++){
+//     sum *= x[i]
+//   }
+//   return sum
 // }
