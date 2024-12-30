c97964c1888946e68594c24f8772e078ee6af2af
Kenny S <kennethstrunk@yahoo.com>
2024-12-30 13:48:06 -0700

    array practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -1080,3 +1080,22 @@
 // Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
 
 // If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+// function distinct(arr) {
+//   let res = [];
+//    for(let i = 0; i < arr.length; i++){
+//      if(!res.includes(arr[i]))
+//        res.push(arr[i])
+//    }
+//    return res
+//  }
